2e9365b4a893cc7f7198eed24f3c8a80a96da9fc
Nicholas Berlette <nick@berlette.com>
2022-12-08 08:34:42 -0800

    refactor: project style

--- mod.ts
+++ mod.ts
@@ -1,3 +1,7 @@
-export { assert, is, is as default } from "./is.ts";
-
-export type { Assert, Assertions, TypeName } from "./is.ts";
+/**
+ * @module 🦕 Deno typechecks and assertions with ease 💪
+ * @see https://deno.land/x/dis@0.2.0/README.md for examples and feature docs
+ * @see https://deno.land/x/dis@0.2.0/mod.ts?doc for API docs (auto-generated)
+ */
+export { assert, default, default as is } from "./is.ts";
+export type { Assert, TypeName } from "./is.ts";
--- node.d.ts
+++ node.d.ts
@@ -49,3 +49,41 @@ declare interface ObservableLike {
 }
 
 declare type Falsy = false | 0 | 0n | "" | null | undefined;
+
+interface SymbolConstructor {
+  /**
+   * A method that describes the object as `Observable` and returns an object
+   * with a `subscribe` method. That method accepts an `observer` for its
+   * single argument, which in turn accepts a arbitrary `value` argument. The
+   * subscribe method may return an `unsubscribe` method to run as a cleanup
+   * or teardown routine when the observable is disposed.
+   *
+   * @see https://github.com/tc39/proposal-observable for more details.
+   * @see {@linkcode ObservableLike} for the interface this matches.
+   */
+  readonly observable: unique symbol;
+}
+
+/**
+ * Matches a value that is like an Observable.
+ * @see https://github.com/tc39/proposal-observable
+ */
+interface ObservableLike {
+  subscribe(observer: (value: unknown) => void): void;
+  [Symbol.observable](): ObservableLike;
+}
+
+/**
+ * The "Printable" Primitives - `string`, `number`, `boolean`, `bigint` - are
+ * the subset of the Primitive types that can be printed in Template Literal
+ * types (a feature of TypeScript 4.1+).
+ *
+ * _Technically_ `null` and `undefined` are also printable, but only as the
+ * literal strings `"null"` and `"undefined"`, respectively. As such, they
+ * are not included in this type.
+ *
+ * @see {@linkcode MaybePrintable} if you need to include `null` and `undefined` in the Printable type for your use case.
+ */
+declare type Printable = NonNullable<Exclude<Primitive, symbol>>;
+
+declare type MaybePrintable = Exclude<Primitive, symbol>;
