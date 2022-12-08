72559ed66f7b52524bfa77b3fd7cc68a36bd5ab1
Nicholas Berlette <nick@berlette.com>
2022-12-08 08:31:15 -0800

    refactor: assert.ts

--- assert.ts
+++ assert.ts
@@ -11,55 +11,88 @@ import type {
   SetIterator,
   TypedArray,
 } from "./types.ts";
+import { is } from "./is.ts";
 
 export enum AssertionTypeDescription {
   class_ = "Class",
-  numericString = "string with a number",
+
+  nullish = "null or undefined (nullish)",
   nullOrUndefined = "null or undefined",
-  nullish = "nullish (null or undefined)",
-  iterable = "Iterable",
-  asyncIterable = "AsyncIterable",
-  nativePromise = "native Promise",
-  urlString = "string with a URL",
-  url = "URL instance or a string with a URL",
+
   truthy = "truthy",
   falsy = "falsy",
-  nan = "NaN",
   primitive = "primitive",
-  integer = "integer",
-  safeInteger = "integer",
+  printable = "printable primitive",
+
   plainObject = "plain object",
+
   arrayLike = "array-like",
   typedArray = "TypedArray",
-  domElement = "HTMLElement",
-  nodeStream = "Node.js Stream",
-  infinite = "infinite number",
+
   entry = "tuple-pair entry",
   entries = "tuple-pair entries",
+
+  iterable = "Iterable",
+  asyncIterable = "AsyncIterable",
+
+  key = "key exists in the given object",
+  value = "value exists in the given object",
+  enumKey = "key exists in the given enum",
+  enumCase = "value exists in the given enum",
+
+  nativePromise = "native Promise",
+
+  urlSearchParams = "URLSearchParams instance",
+  urlInstance = "URL instance",
+  urlString = "string with a URL",
+  url = "URL instance or a string with a URL",
+  headers = "Headers instance",
+  response = "Response instance",
+  request = "Request instance",
+  formData = "FormData instance",
+  blob = "Blob instance",
+  file = "File instance",
+  fileInfo = "FileInfo instance",
+  namespaceModule = "module imported via `import * as ...` syntax",
+
+  domElement = "HTMLElement",
+  svgElement = "SVGElement",
+  nodeStream = "Node.js Stream",
+  nodeBuffer = "Node.js Buffer",
+  denoBuffer = "Deno Buffer",
+  buffer = "Buffer (Node.js or Deno)",
+
   sparseArray = "sparse array",
   emptyArray = "empty array",
-  nonEmptyArray = "non-empty array",
-  emptyString = "empty string",
   whitespace = "whitespace string",
+  emptyString = "empty string",
   emptyStringOrWhitespace = "empty string or whitespace",
+  emptyObject = "empty object",
+  emptySet = "empty Set",
+  emptyMap = "empty Map",
+
+  nonEmptyArray = "non-empty array",
   nonEmptyString = "non-empty string",
   nonEmptyStringAndNotWhitespace = "non-empty string and not whitespace",
-  emptyObject = "empty object",
   nonEmptyObject = "non-empty object",
-  emptySet = "empty set",
-  nonEmptySet = "non-empty set",
-  emptyMap = "empty map",
-  nonEmptyMap = "non-empty map",
+  nonEmptySet = "non-empty Set",
+  nonEmptyMap = "non-empty Map",
 
+  nan = "NaN (not a number)",
+  numericString = "string with a number",
+  safeInteger = "integer (safe)",
   evenInteger = "even integer",
   oddInteger = "odd integer",
-
-  directInstanceOf = "T",
+  infinite = "infinite number (+Infinity or -Infinity)",
   inRange = "in range",
+  integer = "integer",
 
-  any = "predicate returns truthy for any value",
-  all = "predicate returns truthy for all values",
-  none = "predicate returns falsy for all values / truthy for none",
+  instanceOf = "instanceof T",
+  directInstanceOf = "instanceof T",
+
+  any = "predicate is truthy for any one value",
+  all = "predicate is truthy for **all** of the values",
+  none = "predicate is truthy for **none** of the values",
   some = any,
   every = all,
 }
@@ -67,33 +100,23 @@ export enum AssertionTypeDescription {
 /**
  * Type assertions have to be declared with an explicit type.
  */
-export interface Assertions {
+export interface assert {
   string: (value: unknown) => asserts value is string;
   number: (value: unknown) => asserts value is number;
   bigint: (value: unknown) => asserts value is bigint;
   boolean: (value: unknown) => asserts value is boolean;
   symbol: (value: unknown) => asserts value is symbol;
   numericString: (value: unknown) => asserts value is string;
-
   array: <T = unknown>(
     value: unknown,
     assertion?: (element: unknown) => asserts element is T,
   ) => asserts value is T[];
-
-  // Unknowns.
   undefined: (value: unknown) => asserts value is undefined;
   nullOrUndefined: (value: unknown) => asserts value is null | undefined;
   nullish: (value: unknown) => asserts value is null | undefined;
   null: (value: unknown) => asserts value is null;
-  /** @deprecated use {@linkcode assert.null} instead */
-  null_: (value: unknown) => asserts value is null;
-
   function: (value: unknown) => asserts value is Function;
-  /** @deprecated use {@linkcode assert.function} instead */
-  function_: (value: unknown) => asserts value is Function;
   class: <P = unknown>(value: unknown) => asserts value is Class<P>;
-  /** @deprecated use {@linkcode assert.class} instead */
-  class_: <P = unknown>(value: unknown) => asserts value is Class<P>;
 
   iterable: <T = unknown>(value: unknown) => asserts value is Iterable<T>;
   asyncIterable: <T = unknown>(
@@ -113,7 +136,6 @@ export interface Assertions {
   regex: (value: unknown) => asserts value is RegExp;
   date: (value: unknown) => asserts value is Date;
   error: (value: unknown) => asserts value is Error;
-
   map: <Key = unknown, Value = unknown>(
     value: unknown,
   ) => asserts value is Map<Key, Value>;
@@ -127,7 +149,6 @@ export interface Assertions {
   weakRef: <T extends object = object>(
     value: unknown,
   ) => asserts value is WeakRef<T>;
-
   typedArray: (value: unknown) => asserts value is TypedArray;
   int8Array: (value: unknown) => asserts value is Int8Array;
   uint8Array: (value: unknown) => asserts value is Uint8Array;
@@ -140,19 +161,16 @@ export interface Assertions {
   float64Array: (value: unknown) => asserts value is Float64Array;
   bigInt64Array: (value: unknown) => asserts value is BigInt64Array;
   bigUint64Array: (value: unknown) => asserts value is BigUint64Array;
-
   arrayBuffer: (value: unknown) => asserts value is ArrayBuffer;
   sharedArrayBuffer: (value: unknown) => asserts value is SharedArrayBuffer;
   dataView: (value: unknown) => asserts value is DataView;
   buffer: (value: unknown) => asserts value is Buffer;
-
   object: <Key extends keyof any = string, Value = unknown>(
     value: unknown,
   ) => asserts value is Record<Key, Value>;
   namespaceModule: (value: unknown) => asserts value is Module;
   mapIterator: (value: unknown) => asserts value is MapIterator;
   setIterator: (value: unknown) => asserts value is SetIterator;
-
   propertyKey: (value: unknown) => asserts value is PropertyKey;
   propertyDescriptor: <T = unknown>(
     value: unknown,
@@ -163,7 +181,80 @@ export interface Assertions {
   dataDescriptor: <T = unknown>(
     value: unknown,
   ) => asserts value is DataDescriptor<T>;
+  exact: <T extends Record<string, unknown>, U extends T>(
+    value: unknown,
+    shape: T,
+  ) => asserts value is U;
 
+  /**
+   * Determines if an object is a **subset** of another (the "shape" object).
+   * The target object **may not** have any keys that aren't present in the
+   * shape. However, it does not need to have ***all*** of the shape's keys.
+   *
+   * @template T
+   * @param shape The object used as a schema shape to compare against
+   * @param obj The target object to inspect
+   * @returns `true` if the target is a subset of the shape, `false` otherwise
+   *
+   * @see {@linkcode is.superset} for a typecheck that is the opposite of this.
+   *
+   * @example
+   * ```ts
+   * is.assert.subset(
+   *   { a: 0, b: 0 }, // the schema/shape object
+   *   { a: 1, b: 2, c: 3 }, // the target object
+   * ) // => ❌ target has keys not defined in shape!
+   * ```
+   *
+   * @example
+   * ```ts
+   * is.assert.subset(
+   *   { a: 1, b: 2, c: 3 }, // shape
+   *   { a: 1, c: 3 }, // target
+   * ) // => ✔️ target is missing `b`, but all other keys are in the shape!
+   * ```
+   */
+  subset: <T extends Record<string, unknown>>(
+    value: unknown,
+    shape: T,
+  ) => asserts value is {
+    [K in keyof T as (typeof value)[K] extends never ? never : K]: unknown;
+  };
+
+  /**
+   * Determines if an object is a **superset** of a given "shape". The object
+   * must have **all** of the keys in the shape, but (unlike the
+   * {@link is.subset} typecheck) it may also have other keys/properties that
+   * are not defined in the shape object. Another way to think of this is that
+   * the target object is an **_extension_** of the shape object.
+   *
+   * @template T
+   * @param shape The object used as a "schema" to compare against
+   * @param obj The target object to inspect
+   * @returns `true` if the target is a superset of the shape, `false` otherwise
+   *
+   * @see {@linkcode assert.subset} for a typecheck that is the opposite of this.
+   *
+   * @example
+   * ```ts
+   * is.assert.superset(
+   *   { a: 0, b: 0 }, // the schema/shape object
+   *   { a: 1, b: 2, c: 3 }, // the target object
+   * ) // => ✔️ target has all the keys as required by the shape
+   * ```
+   *
+   * @example
+   * ```ts
+   * is.assert.superset(
+   *   { a: 1, b: 2, c: 3 }, // shape
+   *   { a: 1, c: 3, d: 4 }, // target
+   * ) // => ❌ target has an unknown key not defined by the shape!
+   * ```
+   */
+  superset: <T extends Record<string, unknown>>(
+    value: unknown,
+    shape: T,
+  ) => asserts value is { [K in keyof T | string]: unknown };
   key: <T extends readonly unknown[] | Record<PropertyKey, unknown> = {}>(
     value: unknown,
     target: T,
@@ -180,12 +271,10 @@ export interface Assertions {
     value: unknown,
     targetEnum: T,
   ) => asserts value is T[keyof T];
-
   plainObject: <Value = unknown>(
     value: unknown,
   ) => asserts value is Record<string, Value>;
   arrayLike: <T = unknown>(value: unknown) => asserts value is ArrayLike<T>;
-
   entry: <K = unknown, V = unknown>(
     value: unknown,
   ) => asserts value is readonly [K, V];
@@ -238,7 +327,6 @@ export interface Assertions {
   oddInteger: (value: number) => asserts value is number;
   inRange: (value: number, range: number | number[]) => asserts value is number;
   infinite: (value: unknown) => asserts value is number;
-
   directInstanceOf: <T>(
     instance: unknown,
     class_: Class<T>,
@@ -247,8 +335,6 @@ export interface Assertions {
     instance: unknown,
     class_: Class<T>,
   ) => asserts instance is T;
-
-  // Variadic functions.
   any: (
     predicate: Predicate | Predicate[],
     ...values: unknown[]
@@ -275,17 +361,65 @@ export interface AssertOptions {
   multipleValues?: boolean;
 }
 
-export interface Assert extends Assertions {
+type DeprecatedMethods = ""; // `${"null" | "function" | "class"}_`;
+type ExcludedMethods =
+  | DeprecatedMethods
+  | "assert"
+  | "prototype"
+  | "constructor"
+  | "arguments"
+  | "caller"
+  | "callee"
+  | "name";
+
+type is = Required<typeof is>;
+type TypeCheckNames = Exclude<keyof is, ExcludedMethods>;
+type TypeCheckMethods = is[TypeCheckNames];
+
+// assemble a list of assertions based on the available is.{method} typechecks
+// (with a teeny bit of inference magic to extract types for args and return)
+type CreateAssertionMethods<
+  Source,
+  Keys extends keyof Source,
+  Optional extends Keys = never,
+> = PartialByKeys<
+  {
+    [K in Keys]: (
+      Source[K] extends (
+        (value: unknown, ...args: infer A extends unknown[]) => value is infer R
+      ) ? (
+          (value: unknown, ...rest: A) => asserts value is R
+        )
+        : never
+    );
+  },
+  Optional
+>;
+
+export type AssertionMethods = Omit<
+  CreateAssertionMethods<is, TypeCheckNames>,
+  | "regex"
+  | "fn"
+  | "falsey"
+  | "module"
+  | "nodeBuffer"
+  | "denoBuffer"
+  | "NaN"
+  | "numeric"
+  | "boxedPrimitive"
+>;
+
+export interface Assert extends assert {
   (expression: boolean, message?: string): asserts expression;
   (
-    expression: boolean,
-    value: unknown,
-    message: string,
-  ): asserts expression;
+    condition: boolean,
+    valueReceived: unknown,
+    errorMessage: string,
+  ): asserts condition;
   (
     expression: boolean,
-    received: unknown,
-    expected: string,
+    valueReceived: unknown,
+    valueExpected: unknown,
     options?: AssertOptions,
   ): asserts expression;
 }
